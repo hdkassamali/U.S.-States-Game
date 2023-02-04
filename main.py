6cbf67ad9045d2a45b99107897a63b3a0c07e0aa
hdkassamali <122648642+hdkassamali@users.noreply.github.com>
2023-02-03 17:31:19 -0800

    List comprehension update on line 18

--- main.py
+++ main.py
@@ -15,13 +15,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in all_states:
-            if state not in guessed_states:
-                states_to_learn.append(state)
+        states_to_learn = [state for state in all_states if state not in guessed_states]
         states_data = pandas.DataFrame(states_to_learn)
         states_data.to_csv("states_to_learn.csv")
         break
+
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
